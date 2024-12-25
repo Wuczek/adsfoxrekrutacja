4be7a9cc283ce6db59ad2dafe7a9dbbbbf8cfe28
Kamil Wójtowicz <93767137+Wuczek@users.noreply.github.com>
2024-12-25 20:02:53 +0100

    Revert "Create frontend for CRUD app."

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,27 +5,6 @@ export interface User {
     email_verified_at?: string;
 }
 
-export interface Channel {
-    id: number;
-    name: string;
-    client_count: number;
-    created_at?: Date;
-    updated_at?: Date;
-}
-
-export interface ChannelList extends Array<Channel> {
-}
-
-export interface PieData {
-    labels: string[];
-    datasets: {
-        label: string
-        data: number[]
-        backgroundColor: string[]
-        hoverOffset: number
-    }[];
-}
-
 export type PageProps<
     T extends Record<string, unknown> = Record<string, unknown>,
 > = T & {
